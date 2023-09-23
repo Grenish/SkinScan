f4a242e025e0d570c92d758641e80bb0a11c3a15
Grenish rai <ariestheracer@gmail.com>
2023-09-24 00:22:03 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
+import AfterHero from "./AfterHero";
 import Features from "./Features";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Testimonials from "./Testimonials";
 
-export { Features, Footer, Hero, Navbar, Testimonials };
+export { Features, Footer, Hero, Navbar, Testimonials, AfterHero };
