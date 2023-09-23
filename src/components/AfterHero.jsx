cc61cdb64fe7f82dda82b2076be9497b4a50eeff
Grenish rai <ariestheracer@gmail.com>
2023-09-24 00:21:57 +0530

    Create AfterHero.jsx

---
+++ src/components/AfterHero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { skinscan2 } from "../assets";
+
+const AfterHero = () => {
+  return (
+    <div className="">
+      <div className="">
+        <img src={skinscan2} alt="" className="" />
+      </div>
+    </div>
+  );
+};
+
+export default AfterHero;
