62140bd1610cd64cf4dd1e59103187dc23ad6a70
Grenish rai <ariestheracer@gmail.com>
2023-09-25 02:49:37 +0530

    test import

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import googlePlay from "./googlePlay.svg";
 import appStore from "./appStore.svg";
 import skinscan1 from "./skinscan1.png";
 import skinscan2 from "./skinscan2.png";
+import mainScreen from "./main-screen.png";
 
-export { skinscanLogo, googlePlay, appStore, skinscan1, skinscan2 };
+export { skinscanLogo, googlePlay, appStore, skinscan1, skinscan2, mainScreen };
