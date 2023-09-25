683e83c9fcb76c022e5d308e95392a7edeee1b83
Grenish rai <ariestheracer@gmail.com>
2023-09-25 06:02:18 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,7 @@ import appStore from "./appStore.svg";
 import skinscan1 from "./skinscan1.png";
 import skinscan2 from "./skinscan2.png";
 import mainScreen from "./main-screen.png";
+import skinscanAnimation from "./skinscan-animation.mp4";
+import featureBackground from "./featureBackground.jpg";
 
-export { skinscanLogo, googlePlay, appStore, skinscan1, skinscan2, mainScreen };
+export { skinscanLogo, googlePlay, appStore, skinscan1, skinscan2, mainScreen, skinscanAnimation, featureBackground };
