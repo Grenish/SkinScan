0c0be99276c21e4ee6d23e9de0899415e3776293
Grenish rai <107925840+Grenish@users.noreply.github.com>
2023-09-26 13:19:46 +0530

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <div>
-      <div className="flex justify-center items-center p-5">
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/o5cfkbEf8iw?si=tenJFaJbg6LYBym-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
+      <div class="flex justify-center items-center p-5">
+    <iframe class="w-full h-screen" src="https://www.youtube.com/embed/o5cfkbEf8iw?si=tenJFaJbg6LYBym-" title="Educational YouTube video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" aria-label="YouTube video player"></iframe>
+</div>
     </div>
   )
 }
